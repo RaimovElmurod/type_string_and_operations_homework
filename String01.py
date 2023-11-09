830f6fb47dfcd94235accc0bde082aa7b01125e6
elmurod <elmurodraimov764@gmail.com>
2023-11-09 20:54:13 +0500

    len

--- String01.py
+++ String01.py
@@ -1,8 +1,5 @@
 def main():
-    """"Return an optional string of length eight
-    Args:
-        None
-    Returns:
-        str: return answer.
-    """
-    return 
+    a = '12345678'
+    b = 'aaaazzzz'
+    return len(a,),a, len(b),b
+print(main())
