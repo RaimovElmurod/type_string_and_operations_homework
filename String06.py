0cfdedd93e68475c0e5552dd56e7d010416c474e
elmurod <elmurodraimov764@gmail.com>
2023-11-10 10:35:33 +0500

    code

--- String06.py
+++ String06.py
@@ -1,10 +1,3 @@
 def main(s,n):
-    """
-    s string is given. repeat it n times and return the resulting string.
-    Args:
-        s: str
-        n: int
-    Returns:
-        str: return answer.
-    """
-    return 
+    return s*n
+print(main("code", 3))
